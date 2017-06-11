57106f5d06b879608bf8370089279f3249bdd3f0
umashankar somasekar <umashankar somasekar>
2017-06-11 19:54:44 +0530

    new changes

--- p2pnode/myexpress.js
+++ p2pnode/myexpress.js
@@ -1,7 +1,25 @@
 var express = require('express');
-
+var cors = require('cors')
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 app.listen(3000, function() {
     console.log("Server is running......");
+});
+
+app.get('/', function(req, res) {
+    res.send({ "name": "umashankar", "email": "uma.s.shankar@gmail.com" })
+});
+
+app.post('/testmethod', function(req, res) {
+
+    console.log(req.body)
+
+    res.send({ "name": "umashankar", "email": "uma.s.shankar@gmail.com" });
 });
